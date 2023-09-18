9a23e5fe8c5e202deebbb320d5d0da1167df92ef
g0lgi <epatrickk0505@gmail.com>
2023-09-18 23:31:44 +0700

    TUGAS 3 BERES INSHALLAH

---
+++ main/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=64, name='name')
+    amount = models.IntegerField(name='amount')
+    price = models.FloatField(name='price')
+    date_added = models.DateTimeField(name='date_added', auto_now_add=True)
+    description = models.TextField(max_length=256, name='description')

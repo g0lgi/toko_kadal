4e62ae1513c4254fff0e5d67f1e75db655dffe63
g0lgi <epatrickk0505@gmail.com>
2023-09-25 23:32:21 +0700

    TUGAS 4 BERES WOO

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     name = models.CharField(max_length=64, name='name')
@@ -6,3 +7,4 @@ class Product(models.Model):
     price = models.FloatField(name='price')
     date_added = models.DateTimeField(name='date_added', auto_now_add=True)
     description = models.TextField(max_length=256, name='description')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
